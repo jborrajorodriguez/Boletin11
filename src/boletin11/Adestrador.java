11b821e28facb3ecd8b06bae6138b9483f89bf33
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-11-23 13:48:23 +0100

    Clase adestrador inicio proyecto

---
+++ src/boletin11/Adestrador.java
@@ -0,0 +1,26 @@
+package boletin11;
+
+/**
+ * @author Juan Borrajo Rodriguez Nº5937
+ */
+public class Adestrador extends Seleccion {
+
+    private String idFederacion;
+
+    public Adestrador() {
+    }
+
+    public Adestrador(String idFederacion,int id,int idade,String nome,String apelido) {
+        super(id,idade,nome,apelido);
+        this.idFederacion=idFederacion;
+    }
+
+    public void dirixirPartido() {
+
+    }
+
+    public void dirixirAdestramento() {
+
+    }
+
+}
