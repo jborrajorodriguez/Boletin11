9e84f745bfefaef9c52680a4a111d9ae23c95ba9
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-11-23 13:53:20 +0100

    Set y Get realizados

--- src/boletin11/Seleccion.java
+++ src/boletin11/Seleccion.java
@@ -18,6 +18,38 @@ public class Seleccion {
         this.apelido=apelido;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id=id;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade=idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome=nome;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido=apelido;
+    }
+
     public void viaxar() {
         System.out.println("viaxa a selecion");
     }
